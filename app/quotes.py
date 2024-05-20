85d89b89415743c3b44efab8ef9d083aeef9705c
Ryan <pakr@oregonstate.edu>
2024-05-20 16:50:34 -0700

    added files for MicroserviceA

---
+++ app/quotes.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/get_quote')
+def get_quote():
+    with open('golf_quotes.txt', 'r') as file:
+        quotes = file.readlines()
+    random_quote = random.choice(quotes).strip()
+    return jsonify({"quote": random_quote})
+
+if __name__ == "__main__":
+    app.run(port=5002)
