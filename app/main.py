15118007254bc94cfbc8bdb92713a04c943f7f51
Ryan <pakr@oregonstate.edu>
2024-05-20 16:54:50 -0700

    added implemenation for the quote microservice

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from user_profiles import UserProfilesPage
 from stats import StatsPage
+import requests  # Import requests for HTTP communication
 
 
 class GolfApp(tk.Tk):
@@ -22,6 +23,15 @@ class GolfApp(tk.Tk):
         self.main_screen.pack_forget()
         self.stats_page.pack(fill=tk.BOTH, expand=True)
 
+    def get_inspirational_quote(self):
+        response = requests.get('http://localhost:5002/get_quote')
+        if response.status_code == 200:
+            quote_data = response.json()
+            inspirational_quote = quote_data['quote']
+            tk.messagebox.showinfo("Quote of the Day", inspirational_quote)
+        else:
+            tk.messagebox.showerror("Error", "Failed to fetch quote.")
+
 
 class MainScreen(tk.Frame):
     def __init__(self, master):
@@ -32,6 +42,9 @@ class MainScreen(tk.Frame):
 
         self.stats_button = tk.Button(self, text="My Stats", command=master.show_stats)
         self.stats_button.pack()
+
+        self.quotes_button = tk.Button(self, text="Quote of the Day", command=master.get_inspirational_quote)
+        self.quotes_button.pack()
 
 
 if __name__ == "__main__":
